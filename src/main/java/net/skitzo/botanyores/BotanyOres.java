c60a223e5b5a3e6a931f1b0f3b6b4802d829e0f9
JustSkitzo <ryanfielcontact@gmail.com>
2023-07-24 11:58:15 -0400

    Initial Commit

---
+++ src/main/java/net/skitzo/botanyores/BotanyOres.java
@@ -0,0 +1,16 @@
+package net.skitzo.botanyores;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BotanyOres implements ModInitializer {
+	public static final String MOD_ID = "botanyores";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/skitzo/botanyores/BotanyOresClient.java
@@ -0,0 +1,10 @@
+package net.skitzo.botanyores;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BotanyOresClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
